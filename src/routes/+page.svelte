3295f4ca557f34ce2f20a370ad15d26aea502620
zaki.syed@ecozensolutions.com <zaki.syed@ecozensolutions.com>
2025-06-27 18:47:46 +0530

    Changed Layout and Message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to Four Horsemen, by Zaki</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="w-full h-full justify-center items-center">
+    <h1>Welcome to the Eye, by Imaad, Zohra, Zoya and Zaki</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
