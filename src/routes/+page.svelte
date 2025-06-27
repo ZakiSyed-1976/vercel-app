c0d12a38f20219b8881801f8ccc7abb05e6850f0
zaki.syed@ecozensolutions.com <zaki.syed@ecozensolutions.com>
2025-06-27 18:30:19 +0530

    Changed message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit Deployed on Vercel by Zaki</h1>
+<h1>Welcome to Four Horsemen, by Zaki</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
