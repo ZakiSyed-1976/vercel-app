983ad0a15ce2d5aba48ad72836a9fd18f437ec08
zaki.syed@ecozensolutions.com <zaki.syed@ecozensolutions.com>
2025-06-28 14:44:26 +0530

    Simple change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
-<div class="w-full h-full justify-center items-center">
-    <h1>Welcome to the Eye, by Imaad, Zohra, Zoya and Zaki</h1>
-    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="w-full flex justify-center items-center">
+
+
+<h1>Welcome to the Eye, by Imaad, Zohra, Zoya and Zaki</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 </div>
